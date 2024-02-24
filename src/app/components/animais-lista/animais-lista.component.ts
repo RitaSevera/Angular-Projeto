ed8796d10c72c7a3f72b61ce31b20d61859db6be
Rita Severa <ritasevera@hotmail.com>
2024-02-24 21:40:06 +0000

    Final

--- src/app/components/animais-lista/animais-lista.component.ts
+++ src/app/components/animais-lista/animais-lista.component.ts
@@ -4,22 +4,25 @@ import { Animais } from '../../models/animais';
 import { AnimaisService } from '../../services/animais.service';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterLink } from '@angular/router';
+import { Tutores } from '../../models/tutores';
+import { TutoresService } from '../../services/tutores.service';
 
 @Component({
   selector: 'app-animais-lista',
   standalone: true,
   imports: [HttpClientModule, RouterLink],
-  providers: [AnimaisService],
+  providers: [AnimaisService, TutoresService],
   templateUrl: './animais-lista.component.html',
   styleUrl: './animais-lista.component.scss'
 })
 export class AnimaisListaComponent {
 
   animais?: Animais [];
+  tutores: any[] = [];
 
   animaisSubscription?: Subscription;
 
-  constructor(private animaisService: AnimaisService) {}
+  constructor(private animaisService: AnimaisService, private tutoresService : TutoresService) {}
 
   ngOnInit() {
     this.animaisSubscription = this.animaisService.getAll().subscribe({
@@ -30,10 +33,23 @@ export class AnimaisListaComponent {
         console.error('Erro ao carregar os dados!', err);
       },
     });
+    this.tutoresService.getAll().subscribe({
+      next: (tutores) => {
+        this.tutores = tutores;
+      },
+      error: (err) => {
+        console.error('Erro ao carregas informação', err);
+      },
+    });
   }
 
   ngOnDestroy(){
     this.animaisSubscription?.unsubscribe();
+  }
+
+  nomeTutor(tutorId : string): string {
+    const tutor = this.tutores?.find (a => a.id === tutorId);
+    return tutor? tutor.nome : 'Tutor não encontrado'
   }
 
 }
--- src/app/components/marcacoes-lista/marcacoes-lista.component.ts
+++ src/app/components/marcacoes-lista/marcacoes-lista.component.ts
@@ -4,22 +4,27 @@ import { MarcacoesService } from '../../services/marcacoes.service';
 import { Subscription } from 'rxjs';
 import { Marcacao } from '../../models/marcacao';
 import { RouterLink } from '@angular/router';
+import { AnimaisService } from '../../services/animais.service';
+import { TutoresService } from '../../services/tutores.service';
 
 @Component({
   selector: 'app-marcacoes-lista',
   standalone: true,
   imports: [HttpClientModule, RouterLink],
-  providers: [MarcacoesService],
+  providers: [MarcacoesService, AnimaisService, TutoresService],
   templateUrl: './marcacoes-lista.component.html',
   styleUrl: './marcacoes-lista.component.scss'
 })
 export class MarcacoesListaComponent {
 
 marcacoes?: Marcacao[];
+animais: any[] = [];
+tutores: any[] = [];
 
 marcacoesSubscription?: Subscription;
 
-  constructor(private marcacoesService: MarcacoesService) {}
+
+  constructor(private marcacoesService: MarcacoesService, private animaisService : AnimaisService, private tutoresService : TutoresService) {}
 
   ngOnInit() {
     this.marcacoesSubscription = this.marcacoesService.getAll().subscribe({
@@ -30,9 +35,35 @@ marcacoesSubscription?: Subscription;
         console.error('Erro ao carregar os dados!', err);
       },
     });
+    this.animaisService.getAll().subscribe({
+      next: (animais) => {
+        this.animais = animais;
+      },
+      error: (err) => {
+        console.error('Erro a carregar informações', err);
+      },
+    });
+    this.tutoresService.getAll().subscribe({
+      next: (tutores) => {
+        this.tutores = tutores;
+      },
+      error: (err) => {
+        console.error('Erro a carregar informações', err);
+      },
+    });
   }
 
   ngOnDestroy() {
     this.marcacoesSubscription?.unsubscribe();
+  }
+
+  nomeAnimal(animalId : string): string {
+    const animal = this.animais?.find (a => a.id === animalId);
+    return animal? animal.nome : 'Animal não encontrado'
+  }
+
+  nomeTutor(tutorId : string): string {
+    const tutor = this.tutores?.find (a => a.id === tutorId);
+    return tutor? tutor.nome : 'Tutor não encontrado'
   }
 }
